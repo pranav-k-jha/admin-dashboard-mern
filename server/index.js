d5e9d2716fe0a444d8eb92d4883c239c5208b994
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-14 16:24:21 -0400

    added basic layout, configuration and connected mongodb

--- server/index.js
+++ server/index.js
@@ -5,9 +5,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+dotenv.config({ path: "../.env" });
 
 // CONFIGURATION
-
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,3 +27,15 @@ app.use(cors());
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
