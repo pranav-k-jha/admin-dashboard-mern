990c1ed9e2f201b36a13e82aa80c30fb22fe9e28
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-14 21:53:42 -0400

    added under construction message

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,9 +1,25 @@
+import { Paper, Typography } from "@mui/material";
 
-import React from "react";
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "80vh", 
+      }}
+    >
+      <div>
+        <Paper>
+          <Typography sx={{ fontSize: "20px", justifyItems: "space-around" }}>
+            👨‍💻 Currently under construction... 🚧 <br />
+          </Typography>
+        </Paper>
+      </div>
+    </div>
+  );
+  
+};
 
-export default Dashboard
+export default Dashboard;
